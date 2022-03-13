75560da259d8549bc8bb3ee4b2c8905aee974ceb
Emma Scott <emmascotttech@gmail.com>
2022-03-13 11:11:20 +0000

    Add comments to rotate array

--- src/twoPointers/RotateArray.java
+++ src/twoPointers/RotateArray.java
@@ -4,15 +4,21 @@ public class RotateArray {
 
     public void rotate(int[] nums, int k) {
         int num[] = new int[nums.length];
-        k = k % nums.length; int j=0;
+        // ensures k isnt't larger than length
+        k = k % nums.length;
+        // j is pointer for results array
+        int j=0;
+        // first loop puts the values from end of input array to beginning of new array
         for(int i=nums.length-k;i<nums.length;i++){
             num[j] = nums[i];
             j++;
         }
+        // second loop gets the beginning values of input array and adds them after the values from previous loop
         for(int i=0; i<nums.length-k; i++){
             num[j] = nums[i];
             j++;
         }
+        // puts new array values into original array
         for(int i=0; i<nums.length; i++){
             nums[i] = num[i];
         }
