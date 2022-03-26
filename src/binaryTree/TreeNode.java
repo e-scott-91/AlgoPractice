191051a1510967b28cec08de019d4a38c8de1dfe
e-scott-91 <emmascotttech@gmail.com>
2022-03-26 11:16:14 +0000

    Binary tree traversal implementation

---
+++ src/binaryTree/TreeNode.java
@@ -0,0 +1,12 @@
+package binaryTree;
+
+public class TreeNode {
+    String value;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(String value) {
+        this.value = value;
+    }
+}
+
