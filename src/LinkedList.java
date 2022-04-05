599e2ac7026f590ad31ba6a4b5255513996572b6
e-scott-91 <emmascotttech@gmail.com>
2022-04-05 06:05:41 +0100

    has cycle

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -11,4 +11,20 @@ public class LinkedList {
         }
         return prev;
     }
+
+    public boolean hasCycle(ListNode head) {
+        if(head==null) return false;
+
+        ListNode fast=head.next;
+        ListNode slow=head;
+
+        while(slow != fast){
+            if (fast ==null|| fast.next == null){
+                return false;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return true;
+    }
 }
