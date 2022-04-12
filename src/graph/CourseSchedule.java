62c6cba8bdfec81590008b20fe3fe1d2f0ac52c5
e-scott-91 <emmascotttech@gmail.com>
2022-04-12 05:07:43 +0100

    course schedule

---
+++ src/graph/CourseSchedule.java
@@ -0,0 +1,49 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CourseSchedule {
+
+    List<List<Integer>> adjList;
+    int[] visited;
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        //initialize adjList ans visited array
+        adjList = new ArrayList<>();
+        visited = new int[numCourses];
+
+        //build a graph
+        buildGraph(numCourses, prerequisites);
+
+        //iterate through each vertex
+        for(int i=0; i<numCourses; i++){
+            if(visited[i] == 0) //check if its already been processed or not
+                if(isCycle(i)) //check, is there any cycle that includes this index??
+                    return false; //if yes, then we found cycle,hence can't complete each course
+        }
+        return true; //we went through all vertices, did not find any cycle hece we can complete the course. i.e no two courses are dependent on each other.(DEADLOCk)
+    }
+
+    private void buildGraph(int vertices, int[][] edges){
+        //initialize empty list for every vertex
+        for(int i=0; i<vertices; i++)
+            adjList.add(new ArrayList<Integer>());
+
+        //establish the edges between the vertices
+        for(int i=0; i<edges.length; i++)
+            adjList.get(edges[i][0]).add(edges[i][1]);
+    }
+
+    private boolean isCycle(int vertex){
+        if(visited[vertex] == 2) //we visited this vertex agaain, hence found cycle.
+            return true;
+        visited[vertex] = 2; //set this as, processing vertex.
+        for(int neighbourVertex : adjList.get(vertex)){ // check for its each neighbourVertexs
+            if(visited[neighbourVertex] != 1) //check if neighbourVertex is already processed or not.
+                if(isCycle(neighbourVertex))
+                    return true; //we found cycle in one of the neighbourVertex.
+        }
+        visited[vertex] = 1; //did not find any cycle, hence mark it as done and return false.
+        return false;
+    }
+}
--- src/recursion/CloneGraph.java
+++ src/recursion/CloneGraph.java
@@ -6,34 +6,30 @@ import java.util.HashSet;
 public class CloneGraph {
 
     public Node cloneGraph(Node node) {
-        if (node == null) return null;
-
+        if (node == null) return node;
         return dfsClone(node);
     }
 
-    public Node dfsClone(Node node) {
+    public Node dfsClone(Node root) {
         HashMap<Integer, Node> map = new HashMap<>();
         HashSet<Integer> visited = new HashSet<>();
-        dfsCloneHelper(node, map, visited);
-        return map.get(node.val);
+        dfsCloneHelper(root, map, visited);
+        return map.get(root.val);
     }
 
-    public void dfsCloneHelper(Node node, HashMap<Integer,Node> map, HashSet<Integer> visited) {
-        if (visited.contains(node.val)) {
-            return;
-        }
+    public void dfsCloneHelper(Node node, HashMap<Integer, Node> map, HashSet<Integer> visited) {
+        if (visited.contains(node.val)) return;
 
         Node cloneNode = map.getOrDefault(node.val, new Node(node.val));
         map.put(node.val, cloneNode);
 
         visited.add(node.val);
-
-        for(int i = 0; i < node.neighbors.size(); i++) {
-            Node currentNeighbor = node.neighbors.get(i);
-            Node cloneNeighbor = map.getOrDefault(currentNeighbor.val, new Node(currentNeighbor.val));
-            map.put(currentNeighbor.val, cloneNeighbor);
-            dfsCloneHelper(currentNeighbor, map, visited);
-            cloneNode.neighbors.add(cloneNeighbor);
+        for (int i = 0; i < node.neighbors.size(); i++){
+            Node currentNeighbour = node.neighbors.get(i);
+            Node cloneNeighbour = map.getOrDefault(currentNeighbour.val, new Node(currentNeighbour.val));
+            map.put(currentNeighbour.val, cloneNeighbour);
+            dfsCloneHelper(cloneNeighbour, map, visited);
+            cloneNode.neighbors.add(cloneNeighbour);
         }
     }
 }
