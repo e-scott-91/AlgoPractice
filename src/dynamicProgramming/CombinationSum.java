7c0ea814798a028d924071e8f470ab22b1c55c25
e-scott-91 <emmascotttech@gmail.com>
2022-04-18 10:02:05 +0100

    more problems

--- src/dynamicProgramming/CombinationSum.java
+++ src/dynamicProgramming/CombinationSum.java
@@ -1,16 +1,14 @@
 package dynamicProgramming;
 
 public class CombinationSum {
-
-    int[] dp;
     public int combinationSum4(int[] nums, int target) {
-        dp=new int[target+1];
+        int[] dp = new int[target+1];
 
-        dp[0] =1;
+        dp[0] = 1;
 
-        for(int i = 1 ; i<=target ;i++){
-            for(int n:nums){
-                if(n <= i){
+        for (int i = 1 ; i <= target ;i++) {
+            for (int n: nums) {
+                if (n <= i) {
                     dp[i] += dp[i-n];
                 }
             }
