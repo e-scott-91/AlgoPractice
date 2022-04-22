b16eadff1e34764b51be38b583cf769968a62e72
e-scott-91 <emmascotttech@gmail.com>
2022-04-22 08:25:02 +0100

    course schedule II

---
+++ src/graph/CourseScheduleII.java
@@ -0,0 +1,60 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class CourseScheduleII {
+
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+
+        List<Integer>[] adj = new ArrayList[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            adj[i] = new ArrayList<>();
+        }
+
+        for (int[] elem: prerequisites) {
+            adj[elem[0]].add(elem[1]);
+        }
+
+        ArrayList<Integer> topoSort = new ArrayList<>();
+        int[] status = new int[numCourses];
+
+        for(int i = 0; i < numCourses; i++) {
+            if(isCyclic(topoSort, status, adj, i)) {
+                return new int[0];
+            }
+        }
+
+
+
+        return topoSort.stream().mapToInt(i -> i).toArray();
+
+
+    }
+
+    // 0 means that vertex has not been visited yet, (-1) means that vertex is still being processed, 1 means that  vertex has already been processed.
+    private boolean isCyclic(ArrayList<Integer> topoSort, int[] status, List<Integer>[] adj, int vertex) {
+        if(status[vertex] == -1) {
+            return true;
+        }
+
+        if(status[vertex] == 1) {
+            return false;
+        }
+
+        status[vertex] = -1;
+
+        for(int neighbor: adj[vertex]) {
+            if(isCyclic(topoSort, status, adj, neighbor)) {
+                return true;
+            }
+        }
+
+        status[vertex] = 1;
+        topoSort.add(vertex);
+
+        return false;
+    }
+}
